2c7002161f71bf9f9f70e40dbc4adcba15355394
Dettlaff404 <120651088+Dettlaff404@users.noreply.github.com>
2023-01-26 16:19:59 +0530

    GameManager Updated
    added deal damage function and player health values and resets

--- Final Assessment/Assets/Scripts/GameManagerScript.cs
+++ Final Assessment/Assets/Scripts/GameManagerScript.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
+    public static GameManagerScript thisGameManagerScript; //to access this script in other scripts
+
+    public int maxHealth = 100; //maximum health value of the player
+    public int currentHealth; //current health value of the player
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth; //resetting the health value of the player
+
+        //assigning the game manager script to be referenced in other scripts if its not assigned earlier
+        if (thisGameManagerScript == null)
+        {
+            thisGameManagerScript = this;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //function to deal damage to the player
+    public void DealDamage(int amountOfDamage)
+    {
+        //reducing the current health value of the player by the amount of damage recieved
+        currentHealth = currentHealth - amountOfDamage;
+
+        //making the lowest posible number that the current value could have 0
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
     }
 }
