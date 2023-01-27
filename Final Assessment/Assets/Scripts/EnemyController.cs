2dfa0769b480c9f494ae9fc422ac8aa9417608b1
Dettlaff404 <120651088+Dettlaff404@users.noreply.github.com>
2023-01-27 22:26:35 +0530

    enemyHealth Bar

--- Final Assessment/Assets/Scripts/EnemyController.cs
+++ Final Assessment/Assets/Scripts/EnemyController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public static EnemyController enemyControllerScript; //to reference this script in other scripts
+
     public float enemyMoveSpeed = 1f; //movement spead of the enemy
     public float unitsToMove = 2f; //distance that the enemy will patrol to 
     private float startPos; //starting position of the enemy
@@ -11,7 +13,8 @@ public class EnemyController : MonoBehaviour
 
     public SpriteRenderer enemySprite; //sprite renderer component of the enemy
 
-
+    public float enemyMaxHealth = 100f; //enemy max health value
+    public float enemyCurrentHealth;
 
 
     // Start is called before the first frame update
@@ -20,6 +23,13 @@ public class EnemyController : MonoBehaviour
         //recording the starting position of the enemy for later conditions
         startPos = transform.position.x;
 
+        //assigning this script to be referenced in other scripts
+        if (enemyControllerScript == null)
+        {
+            enemyControllerScript = this;
+        }
+
+        enemyCurrentHealth = enemyMaxHealth; //setting enemy health
 
     }
 
